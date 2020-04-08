5fc493a88c20e464420db02a791c32b87e56732a
barath99 <exploretechsbk@gmail.com>
2020-04-08 23:47:13 +0530

    Interpolation Search

--- Searching and Sorting/interpolationSearch.py
+++ Searching and Sorting/interpolationSearch.py
@@ -36,15 +36,14 @@ def interpolationSearch(arr, n, x):
 
 # Driver Code 
 # Array of items oin which search will be conducted 
-arr = [10, 12, 13, 16, 18, 19, 20, 21, \ 
-				22, 23, 24, 33, 35, 42, 47] 
+arr = [10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47] 
 n = len(arr) 
 
 x = 18 # Element to be searched 
 index = interpolationSearch(arr, n, x) 
 
 if index != -1: 
-	print "Element found at index",index 
+	print ("Element found at index",index )
 else: 
-	print "Element not found"
+	print ("Element not found")
 
